ac256ca0abcb2b67499362d458cde592995e65c5
Rinkeshsingh-byte <rinkeshsingh736@gmail.com>
2026-09-01 18:22:40 +0530

    trim

--- Trim.java
+++ Trim.java
@@ -2,8 +2,8 @@ public class Trim
 {
     public static void main(String args[])
     {
-        String str1= ("Rinkesh$kumar%singh");  
-        String x= str1.replaceAll("[\\$,%]", " ");
+        String str1= ("       Rinkesh          kumar            singh         ");  
+        String x= str1.replaceAll("\\s+", " ").trim();
         System.out.println(x);
     }
 }
