f02675cf7f7f14fcc4ab5aae66183ae656b4839e
Rinkeshsingh-byte <rinkeshsingh736@gmail.com>
2026-09-03 13:58:24 +0530

    Armstrong number using loop

---
+++ practiceforloop.java
@@ -0,0 +1,13 @@
+public class practiceforloop 
+{
+    public static void main(String args[])
+    {
+       String str="Apple";
+       String Target="p";
+    for(int i=0;i<str.length(); i++){
+        if 
+    }
+        
+        System.out.println();
+    }
+}
