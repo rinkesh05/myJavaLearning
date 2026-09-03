7aac618a454412135d86e34f73478716175e6e28
Rinkeshsingh-byte <rinkeshsingh736@gmail.com>
2026-09-03 19:34:15 +0530

    Fibonacci Series

---
+++ Fibonacciseries.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Fibonacciseries  
+{
+   public static void main(String args[])
+   {
+    System.out.println("Program of Fibonacci series");
+    System.out.println("enter the number of terms ");
+       Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=0,b=1,c;
+        System.out.print(a+","+b+",");
+        for(int i=0; i<n-2;i++)
+        {
+            c=a+b;
+            System.out.print(c+",");
+            a=b;
+            b=c;
+        }
+   }    
+}
