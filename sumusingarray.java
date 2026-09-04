e9781880b93761a3c6b9396bd717c411ffbaa3a7
Rinkeshsingh-byte <rinkeshsingh736@gmail.com>
2026-09-04 12:09:14 +0530

    Sum using Array

---
+++ sumusingarray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class sumusingarray 
+{
+    public static void main(String args[])
+    {
+       int A[]={3,9,7,8,12,6,15,5,4,10};
+       int sum=0;
+       for(int i=0;i<A.length;i++)
+       {
+        sum=sum+A[i];
+       
+       }
+          System.out.print(sum);
+    }
+}
