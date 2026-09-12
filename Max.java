0b24cf91165801064869f5ae023b2c1157e329fd
Rinkeshsingh-byte <rinkeshsingh736@gmail.com>
2026-09-12 21:32:37 +0530

    Maximum using method

---
+++ Max.java
@@ -0,0 +1,21 @@
+public class Max
+{
+   static int max(int...A)
+   {
+       if(A.length == 0)
+        return Integer.MIN_VALUE;
+       int max = A[0];
+       for(int i = 1; i < A.length; i++)
+       {
+           if(A[i] > max)
+               max = A[i];
+       }
+       return max;
+   }
+
+    public static void main(String args[])
+    {
+       
+           System.out.println( max(10,22,43,43,2534));
+    }
+}
