ce6828be740cfd167f2fcaa54924fccbccfd1329
Rinkeshsingh-byte <rinkeshsingh736@gmail.com>
2026-09-17 03:07:56 +0530

    customer detail using constructor

--- customer.java
+++ customer.java
@@ -6,14 +6,17 @@ public class customer
     private double price;
     private int quantity;
 
-     public customer()
+      public customer( double i,String n)
      {
-        itemno=2601;
-        name="John";
-        price=2000;
-        quantity=5;
-
+      itemno=i;
+      name=n;
      }
+      public customer(double i,double p)
+     {
+       itemno =i;
+       price = p;
+     }
+
      public customer(double i,String n,double p,int q)
      {
         itemno=i;
@@ -53,12 +56,15 @@ class product
 {      
     public static void main(String args[])
     {
-       customer c1 = new customer();
-       customer c2 = new customer(10502,"rohit",1500,2);
+       customer c1 = new customer(34543,"Mark");
+       customer c2 = new customer(4354,1500);
+       customer c3 = new customer(10502,"rohit",1500,2);
+
 
         c1.setprice(100);
         c1.setquantity(7);
  
+        System.out.println("For c1:");
         System.out.println(c1.getitemno());
         System.out.println(c1.getname());
         System.out.println(c1.getprice());
@@ -67,9 +73,20 @@ class product
         c2.setprice(200);
         c2.setquantity(3);
       
+        System.out.println("For c2:");
         System.out.println(c2.getitemno());
         System.out.println(c2.getname());
         System.out.println(c2.getprice());
          System.out.println(c2.getquantity());
+
+
+         c3.setprice(500);
+        c3.setquantity(10);
+ 
+        System.out.println("For c3:");
+        System.out.println(c3.getitemno());
+        System.out.println(c3.getname());
+        System.out.println(c3.getprice());
+        System.out.println(c3.getquantity());
     }
 }
