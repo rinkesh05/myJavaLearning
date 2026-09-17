cb5233ce675da1881d7dd870fe193ae1244b30e4
Rinkeshsingh-byte <rinkeshsingh736@gmail.com>
2026-09-17 23:07:21 +0530

    inheritance basic

---
+++ test.java
@@ -0,0 +1,52 @@
+
+public class test
+ {
+    public static void main(String[] args) {
+     cylinder c = new cylinder();
+        c.radius = 7;
+        c.height = 10;
+
+        
+        System.out.println("Surface Area: " + c.area());
+        System.out.println("Volume: " + c.volume());
+    }
+}
+
+class circle9
+{
+    public double radius;
+
+    public double area() 
+    {
+        return Math.PI * radius * radius;
+    }
+
+    public double perimeter() 
+    {
+        return 2 * Math.PI * radius;
+    }
+
+    public double circumference() 
+    {
+        return perimeter();
+    }
+}
+
+class cylinder extends circle9
+{
+    public double height;
+
+    
+    @Override
+    public double area() 
+    {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+
+    public double volume()
+     {
+        return Math.PI * radius * radius * height;
+    }
+}
+
+
